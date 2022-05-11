432795dbe049b8a00a1898ba5b4c7c7ac0f9d30e
murilo.sgregorio <murilosenacpi@gmail.com>
2022-05-10 22:11:01 -0300

    Adicionando Room(SQL)

--- app/src/main/java/br/senac/noteapp/data/Note.kt
+++ app/src/main/java/br/senac/noteapp/data/Note.kt
@@ -1,3 +1,12 @@
 package br.senac.noteapp.data
 
-data class Note(var title: String, var desc: String)
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity
+data class Note(
+    @PrimaryKey(autoGenerate = true)
+    var id: Int? =  null,
+    var title: String,
+    var desc: String
+    )
